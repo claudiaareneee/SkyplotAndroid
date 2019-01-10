d615ae85d785213dedcab36fc806605c15b92b6a
Claudia Nelson <claudiaareneee@gmail.com>
2019-01-10 12:15:54 -0600

    added margin requirement for graph

--- app/src/main/java/com/example/claudia/skyplotchart/SkyplotGrid.java
+++ app/src/main/java/com/example/claudia/skyplotchart/SkyplotGrid.java
@@ -7,12 +7,14 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 
 public class SkyplotGrid {
-    private int numberOfCircles = 5;
+    private int numberOfCircles = 3;
     private int numberOfLines = 12;
 
-    Paint paint, textPaint;
-    Rect rect = new Rect();
-    float radius, centerX, centerY;
+    private Paint paint, textPaint;
+    private Rect rect = new Rect();
+    private Rect bounds = new Rect();
+    private int margin = 0;
+    private float radius, centerX, centerY;
 
     public SkyplotGrid(){
         paint = new Paint();
@@ -28,21 +30,22 @@ public class SkyplotGrid {
         textPaint.setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
     }
 
-    public void drawGraph(Canvas canvas) {
-        centerX = canvas.getWidth()/2;
-        centerY = canvas.getHeight()/2;
+    public void setMargin(int margin){
+        this.margin = margin;
+    }
 
-        if(centerX<centerY) radius = centerX;
-        else radius = centerY;
+    public void drawGraph(Canvas canvas) {
+        bounds.set(margin,margin,canvas.getWidth()-margin,canvas.getHeight()-margin);
+
+        centerX = bounds.centerX();
+        centerY = bounds.centerY();
+
+        if(centerX<centerY) radius = centerX - margin;
+        else radius = centerY - margin;
         paint.setStrokeWidth(radius*(float)0.01);
 
         drawCircles(canvas);
         drawLines(canvas);
-    }
-
-    public void setCenter(float centerX, float centerY){
-        this.centerX = centerX;
-        this.centerY = centerY;
     }
 
     private void drawCircles(Canvas canvas){
@@ -72,8 +75,6 @@ public class SkyplotGrid {
 
             currentAngle += angleOfChange;
         }
-
-
     }
 
     private void drawLabel(Canvas canvas, float radians, float startX, float startY){
@@ -81,18 +82,9 @@ public class SkyplotGrid {
         currentAngle +=90;
         if(currentAngle > 360) currentAngle-=360;
 
+        String text = String.format("%.0fº", currentAngle);
 
-        String text = String.format("%.2f", currentAngle);
-
-
-
-//        canvas.save();
-//        canvas.rotate(radians);
-
-        textPaint.getTextBounds(text,0,text.length(),rect);
+//        textPaint.getTextBounds(text,0,text.length(),rect);
         canvas.drawText(text,startX,startY, textPaint);
-
-//        canvas.restore();
     }
-
 }
--- app/src/main/java/com/example/claudia/skyplotchart/SkyplotView.java
+++ app/src/main/java/com/example/claudia/skyplotchart/SkyplotView.java
@@ -2,12 +2,12 @@ package com.example.claudia.skyplotchart;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class SkyplotView extends View {
-
-    private static final String TAG = "SkyplotView";
 
     SkyplotGrid skyplotGrid = new SkyplotGrid();
 
@@ -32,6 +32,7 @@ public class SkyplotView extends View {
     }
 
     private void init(){
+        skyplotGrid.setMargin(80);
     }
 
     @Override
@@ -42,6 +43,18 @@ public class SkyplotView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
+//        //TODO: Remove this paint and rectangle, this is just to position the view.
+//        Paint paint = new Paint();
+//        paint.setAntiAlias(true);
+//        paint.setColor(Color.RED);
+//        paint.setStyle(Paint.Style.FILL);
+//
+//        canvas.drawRect(0,0,getWidth(),getHeight(), paint);
+//        paint.setColor(Color.GREEN);
+//        Float margins = 40f;
+//        canvas.drawRect(margins,margins,getWidth()-margins,getHeight()-margins, paint);
 
         skyplotGrid.drawGraph(canvas);
     }
